51f68415b7dd059d079371d4ff73ece40e3ffbb6
Настя Быкова <ProtocolApogay@gmail.com>
2023-01-28 14:51:04 +0300

    comm

--- prototip/WindowCode.xaml.cs
+++ prototip/WindowCode.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace prototip
 {
@@ -19,11 +20,31 @@ namespace prototip
     /// </summary>
     public partial class WindowCode : Window
     {
+        DispatcherTimer timer= new DispatcherTimer();
         int codec;
         public WindowCode(int code)
         {
             InitializeComponent();
             codec= code;
+            timer.Interval = new TimeSpan(0, 0, 5);
+            timer.Tick += new EventHandler(Timer_Trick);
+            timer.Start();
+        }
+
+        private void tbcode_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (tbcode.Text.Length == 5) 
+            {
+                if (Convert.ToInt32(tbcode.Text) == codec) 
+                {
+                    FrameC.frame.Navigate(new Congratul());
+                }
+            }
+        }
+
+        private void Timer_Trick(object sender, EventArgs e) 
+        {
+            FrameC.frame.Navigate(new Auto());
         }
     }
 }
